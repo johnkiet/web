5b668b578559f6225cc782676044eddf20a22927
johnkiet <ltk08122000@gmail.com>
2021-06-27 15:54:47 +0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewShop.Startup))]
+namespace NewShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
